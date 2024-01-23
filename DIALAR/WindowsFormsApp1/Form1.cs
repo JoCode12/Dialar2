314639e383ce2e28411d72114757ccdbdab0875a
JoCode12 <157454668+JoCode12@users.noreply.github.com>
2024-01-23 15:27:30 +0800

    Add files via upload

---
+++ DIALAR/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public StreamReader R = new StreamReader("C:\\Users\\Administrator\\Desktop\\DIALAR\\Dialar1.txt");
+        public String[] RSource = { };
+
+
+        
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Font = new System.Drawing.Font("bold",40);
+            label1.Text = R.ReadLine();
+    
+        
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
